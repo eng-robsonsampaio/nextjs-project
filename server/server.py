50602904a137b38b429462d79eba4bf15c88e647
eng-robsonsampaio <robsonsampaio90@gmail.com>
2021-11-04 17:42:36 -0300

    Server in python

---
+++ server/server.py
@@ -0,0 +1,24 @@
+import eventlet
+import socketio
+
+from random import randint, randrange
+from time import sleep
+
+sio = socketio.Server(cors_allowed_origins="*")
+app = socketio.WSGIApp(sio)
+
+@sio.event
+def connect(sid, environ):
+    print('Connected: ', sid)
+
+@sio.event
+def disconnect(sid):
+    print('Disconnected: ', sid)
+
+@sio.event
+def request(sid, data):
+    print('request: ', data)
+    sio.emit('response', {'res': data}, to=sid)
+    
+if __name__=='__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 50000)), app)
